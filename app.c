5f33be55dc7d54dda075c3d3706ec68dce6b1e4c
lelembec <chris.lelembe@edu.esiee.fr>
2025-06-01 17:45:45 +0200

    Notify fonctionne

--- app.c
+++ app.c
@@ -34,13 +34,22 @@
 #include "app_log.h"
 #include "temperature.h"
 #include "gatt_db.h"
-
+#include "sl_bt_api.h"
+#include "sl_sleeptimer.h"
 
 // The advertising set handle allocated from Bluetooth stack.
 static uint8_t advertising_set_handle = 0xff;
 float temp;
 float rh;
 int16_t ble_temp;
+
+static sl_sleeptimer_timer_handle_t temp_timer;
+static int timer_count = 0;
+
+#define TEMPERATURE_TIMER_SIGNAL (1 << 0)
+static uint8_t notify_connection = 0xFF;
+
+
 
 
 /**************************************************************************//**
@@ -74,6 +83,15 @@ SL_WEAK void app_process_action(void)
  {
    app_log_info("%s\n", __FUNCTION__);
    temperature_deinit();
+ }
+ static void temperature_timer_callback(sl_sleeptimer_timer_handle_t *handle, void *data)
+ {
+   (void)handle;
+   (void)data;
+
+   timer_count++;
+   sl_bt_external_signal(TEMPERATURE_TIMER_SIGNAL);
+
  }
 
 /**************************************************************************//**
@@ -118,13 +136,16 @@ void sl_bt_on_event(sl_bt_msg_t *evt)
     // This event indicates that a new connection was opened.
     case sl_bt_evt_connection_opened_id:
       app_log_info("%s: Connection_Opened!\n",__FUNCTION__);
+      //uint16_t client_config_flags = evt->data.evt_gatt_server_characteristic_status.client_config_flags;
+      //app_log_info("client_config_flags = 0x%04X  (avant Notify)\n", client_config_flags);
+
 
       break;
 
     // -------------------------------
     // This event indicates that a connection was closed.
     case sl_bt_evt_connection_closed_id:
-      app_log_info("%s: Connection_Closed!\n",__FUNCTION__);
+      //app_log_info("%s: Connection_Closed!\n",__FUNCTION__);
 
       // Generate data for advertising
       sc = sl_bt_legacy_advertiser_generate_data(advertising_set_handle,
@@ -135,6 +156,12 @@ void sl_bt_on_event(sl_bt_msg_t *evt)
       sc = sl_bt_legacy_advertiser_start(advertising_set_handle,
                                          sl_bt_legacy_advertiser_connectable);
       app_assert_status(sc);
+      sl_status_t sc = sl_sleeptimer_stop_timer(&temp_timer);//stop timer
+      //app_log_info("Aucun client connecté pour notifier : status = 0x%04X\n", (unsigned int)sc);
+      app_log_info("%s: Connection_Closed! : status = 0x%04X\n",__FUNCTION__,(unsigned int)sc);
+      notify_connection = 0xFF;
+
+
       break;
 
     ///////////////////////////////////////////////////////////////////////////
@@ -202,12 +229,92 @@ void sl_bt_on_event(sl_bt_msg_t *evt)
         }
       }
 
+    break;
+    }
+    case sl_bt_evt_gatt_server_characteristic_status_id:
+          {
+            uint16_t attr = evt->data.evt_gatt_server_characteristic_status.characteristic;
+            uint8_t status_flags = evt->data.evt_gatt_server_characteristic_status.status_flags;
+            uint16_t client_config_flags = evt->data.evt_gatt_server_characteristic_status.client_config_flags;
+
+             // Vérifie que la notification est activée pour Temperature
+             if (attr == gattdb_temperature && status_flags == sl_bt_gatt_server_client_config) {
+
+               if (client_config_flags == 0x0001) {
+                   app_log_info("Demande de notification pour la temperature\n");
+                   notify_connection = evt->data.evt_gatt_server_characteristic_status.connection;
+
+
+                     timer_count = 0; // reinitialisation count
+                     sl_status_t sc = sl_sleeptimer_start_periodic_timer_ms(
+                       &temp_timer,
+                       1000,                    // 1 seconde
+                       temperature_timer_callback,
+                       NULL,
+                       0,
+                       0
+                     );
+                     app_log_info("Timer demarre : status = 0x%04X\n", (unsigned int)sc);
+                   } else if (client_config_flags == 0x0000) {
+                     app_log_info("Plus de demande de notification pour la temperature. Arret du timer.\n");
+                     notify_connection = 0xFF;
+
+
+                     sl_status_t sc = sl_sleeptimer_stop_timer(&temp_timer);
+                     app_log_info("Timer arrete : status = 0x%04X\n", (unsigned int)sc);
+                   }else{
+                       sl_status_t sc = sl_sleeptimer_stop_timer(&temp_timer);//stop timer
+                       app_log_info("Timer arrete : status = 0x%04X\n", (unsigned int)sc);
+                       notify_connection = 0xFF;
+                   }
+
+             }
+             break;
+          }
+    case sl_bt_evt_system_external_signal_id:
+    {
+      uint32_t signals = evt->data.evt_system_external_signal.extsignals;
+
+      if (signals & TEMPERATURE_TIMER_SIGNAL) {
+        int16_t ble_temp;
+
+        if (temperature_read_ble(&ble_temp)) {
+            int16_t int_part = ble_temp / 100;
+            int16_t frac_part = (ble_temp % 100);
+
+            int16_t temp_data[2]= {0};
+            memcpy(temp_data,&ble_temp,sizeof(int16_t));
+
+            app_log_info("Temperature = %d.%d\xB0""C\n", int_part,frac_part);
+
+            if (notify_connection != 0xFF) {
+              sl_status_t sc = sl_bt_gatt_server_send_notification(
+              notify_connection,
+              gattdb_temperature,
+              sizeof(temp_data),
+              (uint8_t*)temp_data
+            );
+
+            app_log_info("Temperature envoye = %d (%.2f\xB0""C), status = 0x%04X\n",
+                         ble_temp, (double)(ble_temp % 10), (unsigned int)sc);
+          } else {
+            //app_log_info("Aucun client connecté pour notifier\n");
+            sl_status_t sc = sl_sleeptimer_stop_timer(&temp_timer);//stop timer
+            app_log_info("Aucun client connecté pour notifier : status = 0x%04X\n", (unsigned int)sc);
+            notify_connection = 0xFF;
+
+            break;
+
+          }
+        } else {
+          app_log_warning("Lecture température échouée\n");
+          notify_connection = 0xFF;
+        }
+      }
+
       break;
     }
 
-
-
-    break;
 
     // -------------------------------
     // Default event handler.
